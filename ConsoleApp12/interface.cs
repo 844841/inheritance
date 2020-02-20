7990c3644f70cdf5eb2b594290c1a666ace46930
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:13:12 +0530

    Initial commit

---
+++ ConsoleApp12/interface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    // C# program to demonstrate working of 
+    // interface 
+    using System;
+
+    // A simple interface 
+  /*  interface Itask
+    {
+        // method having only declaration 
+        // not definition 
+        void display();
+    }
+
+    // A class that implements interface. 
+    class testClass : Itask
+    {
+
+        // providing the body part of function 
+        public void display()
+        {
+            Console.WriteLine("This is a task");
+        }
+        // Main Method 
+        public static void Main(String[] args)
+        {
+
+            // Creating object 
+            testClass t = new testClass();
+
+            // calling method 
+            t.display();
+        }
+    }*/
+}
+
+ 
+ 
+
+
+
+
+
---
+++ ConsoleApp12/multipleinheritance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class multipleinheritance
+    {
+        class Bike
+        {
+            public void m()
+            {
+
+            }
+            Console.WriteLine("");
+        }
+    }
+}
